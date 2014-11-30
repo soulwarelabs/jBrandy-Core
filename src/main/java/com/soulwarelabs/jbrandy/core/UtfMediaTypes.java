6145d77ef747673b303857e9844dedb8822784cc
IlyaGubarev <ilya.gubarev@gmail.com>
2014-11-30 23:39:04 +0300

    UtfMediaTypes added

---
+++ src/main/java/com/soulwarelabs/jbrandy/core/UtfMediaTypes.java
@@ -0,0 +1,12 @@
+package com.soulwarelabs.jbrandy.core;
+
+public final class UtfMediaTypes {
+
+    public static final String TEXT_HTML = "text/html;charset=utf-8";
+
+    public static final String TEXT_PLAIN = "text/plain;charset=utf-8";
+
+    private UtfMediaTypes() {
+
+    }
+}
