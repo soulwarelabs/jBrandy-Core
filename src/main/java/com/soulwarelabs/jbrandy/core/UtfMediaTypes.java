ae3ea89dfd49d6d1f88317da26d37ff145b7000f
IlyaGubarev <ilya.gubarev@gmail.com>
2014-12-01 13:01:10 +0300

    project structure improved, javadocs updated

--- src/main/java/com/soulwarelabs/jbrandy/core/UtfMediaTypes.java
+++ src/main/java/com/soulwarelabs/jbrandy/core/UtfMediaTypes.java
@@ -1,9 +1,52 @@
+/*
+ * Project:  jBrandy Core
+ * Outline:  jBrandy framework core components
+ *
+ * File:     UtfMediaType.java
+ * Folder:   src/main/java/com/soulwarelabs/jbrandy/core
+ * Revision: 1.01, 30 November 2014
+ * Created:  29 November 2014
+ * Authors:  Ilya Gubarev
+ *
+ * Copyright (c) 2014 Soulware Labs, Ltd.
+ * Contact information is available at "http://www.soulwarelabs.com".
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       "http://www.apache.org/licenses/LICENSE-2.0".
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.soulwarelabs.jbrandy.core;
 
+/**
+ * Media types MIME types with specified UTF-8 encoding.
+ *
+ * @since v1.0.0
+ *
+ * @author Ilya Gubarev
+ * @version 30 November 2014
+ */
 public final class UtfMediaTypes {
 
+    /**
+     * HTML content.
+     *
+     * @since v1.0.0
+     */
     public static final String TEXT_HTML = "text/html;charset=utf-8";
 
+    /**
+     * Plain text.
+     *
+     * @since v1.0.0
+     */
     public static final String TEXT_PLAIN = "text/plain;charset=utf-8";
 
     private UtfMediaTypes() {
--- src/main/java/com/soulwarelabs/jbrandy/core/filters/AbstractSecretary.java
+++ src/main/java/com/soulwarelabs/jbrandy/core/filters/AbstractSecretary.java
@@ -1,4 +1,29 @@
-package com.soulwarelabs.jbrandy.core;
+/*
+ * Project:  jBrandy Core
+ * Outline:  jBrandy framework core components
+ *
+ * File:     AbstractSecretary.java
+ * Folder:   src/main/java/com/soulwarelabs/jbrandy/core/filters
+ * Revision: 1.03, 01 December 2014
+ * Created:  28 November 2014
+ * Authors:  Ilya Gubarev
+ *
+ * Copyright (c) 2014 Soulware Labs, Ltd.
+ * Contact information is available at "http://www.soulwarelabs.com".
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       "http://www.apache.org/licenses/LICENSE-2.0".
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.soulwarelabs.jbrandy.core.filters;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -13,17 +38,52 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public abstract class AbstractSecretaryFilter implements Filter {
+/**
+ * Web filter for resource dispatching.
+ *
+ * @see Filter
+ *
+ * @since v1.0.0
+ *
+ * @author Ilya Gubarev
+ * @version 01 December 2014
+ */
+public abstract class AbstractSecretary implements Filter {
 
+    /**
+     * Filter action.
+     *
+     * @since v1.0.0
+     */
     public static enum Action {
 
+        /**
+         * Forwarding HTTP request.
+         *
+         * @since v1.0.0
+         */
         FORWARD,
 
+        /**
+         * Redirecting HTTP request.
+         *
+         * @since v1.0.0
+         */
         REDIRECT;
     }
 
+    /**
+     * Default filter action.
+     *
+     * @since v1.0.0
+     */
     public static final Action DEFAULT_ACTION = Action.FORWARD;
 
+    /**
+     * Default path converter.
+     *
+     * @since v1.0.0
+     */
     public static final String DEFAULT_CONVERTER = "%s";
 
     private Action action = DEFAULT_ACTION;
@@ -73,50 +133,137 @@ public abstract class AbstractSecretaryFilter implements Filter {
 
     }
 
+    /**
+     * 
+     *
+     * @return 
+     *
+     * @since v1.0.0
+     */
     protected Action getAction() {
         return action;
     }
 
+    /**
+     * 
+     *
+     * @param action 
+     *
+     * @since v1.0.0
+     */
     protected void setAction(Action action) {
         this.action = action;
     }
 
+    /**
+     * 
+     *
+     * @return 
+     *
+     * @since v1.0.0
+     */
     protected String getConverter() {
         return converter;
     }
 
+    /**
+     * 
+     *
+     * @param converter 
+     *
+     * @since v1.0.0
+     */
     protected void setConverter(String converter) {
         this.converter = converter;
     }
 
+    /**
+     * 
+     *
+     * @return 
+     *
+     * @since v1.0.0
+     */
     protected Set<String> getExclusions() {
         return exclusions;
     }
 
+    /**
+     * 
+     *
+     * @param exclusions 
+     *
+     * @since v1.0.0
+     */
     protected void addExclusions(String... exclusions) {
         this.exclusions.addAll(Arrays.asList(exclusions));
     }
 
+    /**
+     * 
+     *
+     * @param exclusion 
+     *
+     * @since v1.0.0
+     */
     protected void removeExclusion(String exclusion) {
         exclusions.remove(exclusion);
     }
 
+    /**
+     * 
+     *
+     * @return 
+     *
+     * @since v1.0.0
+     */
     protected Set<String> getTriggers() {
         return triggers;
     }
 
+    /**
+     * 
+     *
+     * @param triggers 
+     *
+     * @since v1.0.0
+     */
     protected void addTriggers(String... triggers) {
         this.triggers.addAll(Arrays.asList(triggers));
     }
 
+    /**
+     * 
+     *
+     * @param trigger 
+     *
+     * @since v1.0.0
+     */
     protected void removeTrigger(String trigger) {
         triggers.remove(trigger);
     }
 
+    /**
+     * 
+     *
+     * @param original
+     * @return 
+     *
+     * @since v1.0.0
+     */
     protected String convert(String original) {
         return String.format(converter, original);
     }
 
+    /**
+     * 
+     *
+     * @param string
+     * @param pattern
+     * @return 
+     *
+     * @since v1.0.0
+     */
     protected boolean match(String string, String pattern) {
         return string.matches(pattern);
     }
